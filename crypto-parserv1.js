1a4b3a0242ed4d178d7b2972e7fb2c7f0909061b
vernonh64 <94575474+vernonh64@users.noreply.github.com>
2022-02-02 20:49:09 -0500

    task complete 3

--- crypto-parserv1.js
+++ crypto-parserv1.js
@@ -4,10 +4,32 @@ const bitcoin = load('./coin_Bitcoin.csv');
 const eth = load('./coin_Ethereum.csv');
 const doge = load('./coin_Dogecoin.csv');
 
-console.log(bitcoin[6])
+//console.log(bitcoin[2].Marketcap)
+
+function trimmer(array){
+let newArray =[];
+    for(let i = 0; i<array.length; i++ ){
+      let numh = parseFloat(array[i].High).toFixed(8);  
+      let numl = parseFloat(array[i].Low).toFixed(8);
+      newArray.push( {
+            //"SNo": array[i].SNo,
+           //"Symbol": array[i].Symbol,
+            "Symbol": `${array[i].SNo}-${array[i].Symbol}`,
+            "Date": array[i].Date,
+            "High": parseFloat(numh) ,
+            "Low": parseFloat(numl) ,
+            "Marketcap": parseInt(array[i].Marketcap)
+        })
+     
+}
+
+return newArray;
+}
 
 
 
+let doge  = trimmer(doge)
+let Tbitcoin = trimmer(bitcoin)
+let Teth = trimmer(eth)
 
-
-
+console.log(doge[0])
